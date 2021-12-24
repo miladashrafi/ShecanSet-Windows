e2b3aa15ef50e337ed13d68057311eff85fb6154
Milad <milad.ashrafi@gmail.com>
2021-12-24 23:49:35 +0330

    fix

--- Powershell/Shecan OFF.ps1
+++ Powershell/Shecan OFF.ps1
@@ -4,3 +4,16 @@ Write-Host 'Enter Network Adapter Index Number From Above List' -ForegroundColor
 $I = Read-Host -Prompt 'Interface Index'
 Set-DnsClientServerAddress -InterfaceIndex $I -ResetServerAddresses
 Write-Host "Network DNS RESET Successfully."
+
+
+$inputFound = $false
+Do {
+    $I = Read-Host -Prompt 'Interface Index'
+    
+    If ($I -ne "" -and $I -ne $null) {
+        Set-DnsClientServerAddress -InterfaceIndex $I -ResetServerAddresses
+        Write-Host "Network DNS RESET Successfully."
+        $inputFound = $true
+    }
+}
+While ($inputFound -eq $false)
--- Powershell/Shecan ON.ps1
+++ Powershell/Shecan ON.ps1
@@ -3,13 +3,26 @@ $DnsElementList = $WebResponse.AllElements | Where {$_.Class -eq "shecan-dns-ips
 $DnsList = @()
 for ($Index = 1; $Index -le 2; $Index++)
 {
+    Write-Host $DnsElementList[$Index-1].innerText -ForegroundColor Red
     $DnsList += '"' + $DnsElementList[$Index-1].innerText + '"'
 }
 $Value = '(' + ($DnsList -join ',') + ')'
 
 Get-DnsClientServerAddress -AddressFamily IPv4
 Write-Host ""
+Write-Host $Value -ForegroundColor Red
 Write-Host 'Enter Network Adapter Index Number From Above List' -ForegroundColor Yellow
-$I = Read-Host -Prompt 'Interface Index'
-Set-DnsClientServerAddress -InterfaceIndex $I -ServerAddresses $Value
-Write-Host "Network DNS SET Successfully."
+
+$inputFound = $false
+Do {
+    $I = Read-Host -Prompt 'Interface Index'
+    
+    If ($I -ne "" -and $I -ne $null) {
+        Invoke-Expression "Set-DnsClientServerAddress -InterfaceIndex $I -ServerAddresses $Value"
+        Write-Host "Network DNS SET Successfully."
+        $inputFound = $true
+    }
+}
+While ($inputFound -eq $false)
+
+
